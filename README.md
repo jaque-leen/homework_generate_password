e2ec07adf47f41354516ca37501d3b5752842c1a
Jackie <77718180+jaque-leen@users.noreply.github.com>
2021-03-02 02:35:03 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# homework_week03
-Password Generator
+# Homework 3: Password Generator
+
+## Descrption
+
+Application that enables employees to generate random passwords based on criteria that they've selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+
+## Screenshot
+
+The following image shows the web application's apperance and functionality:
+
+![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+
+## Link
+
+xxx
--- script.js
+++ script.js
@@ -1,18 +1,78 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var container = document.getElementById("interface");
+var getBtn = document.querySelector("#getpassword");
+
+//Listen for click event on button
+generateBtn.addEventListener("click", function() {
+  {
+    container.style.display = 'block';
+    generateBtn.style.display = 'none';
+    getBtn.style.display = 'block';
+  }
+}
+);
+
+//user input variables:
+var passLength = document.querySelector('input[name="length"]');
+var lowerCase = document.querySelector('input[name="lowercase"]');
+var upperCase = document.querySelector('input[name="uppercase"]');
+var number = document.querySelector('input[name="number"]');
+var symbol = document.querySelector('input[name="symbol"]');
+
+const key_strings = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: '*;<>()[]{}#$?!^|'
+};
+
+// Add event listener to get button
+getBtn.addEventListener("click", () => {
+	var length = + passLength.value;
+	var activeLower = lowerCase.checked;
+	var activeUpper = upperCase.checked;
+	var activeNumber = number.checked;
+	var activeSymbol = symbol.checked; 
+  
+  generatePassword(activeLower, activeUpper, activeNumber, activeSymbol, length)
+});
 
 // Write password to the #password input
-function writePassword() {
+function generatePassword(lower, upper, num, sym, length) {
+  let MAIN_STRING = "";
+  let PASSWORD = "";
 
-  // generates a random password 
-  var password = generatePassword();
+  const options = {
+    lowerCase: lower,
+    upperCase: upper,
+    number: num,
+    symbol: sym
+  }
+
+  for(i=0;i<Object.keys(options).length;i++){
+		MAIN_STRING += (Object.values(options)[i]) ? key_strings[Object.keys(options)[i]] : "";
+	}
+	
+	if(MAIN_STRING != "" && length > 0){
+		for(i=0;i<length;i++){
+			PASSWORD += MAIN_STRING[Math.floor(Math.random() * MAIN_STRING.length)];
+		}
+		
+		document.getElementById("password").value = password;
+		
+	}else{
+		document.getElementById("password").value = "Define the length!";
+	}
+	
 
   // selects the html elemennt with the id password
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
